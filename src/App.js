1f47c58cd46873fd5f8aef89e10686bec613d24e
TrieuNT <trieunt.work@gmail.com>
2020-12-20 01:24:21 +0700

    Change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nguyễn Tuấn Triều
         </a>
       </header>
     </div>
